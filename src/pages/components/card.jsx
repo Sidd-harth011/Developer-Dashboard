aca6c4471459425a33091fa44de530bb3d68df12
Sidd-harth011 <SIDDHARTHGAUTAM573@GMAIL.COM>
2024-12-11 15:29:30 +0530

    sid

--- src/pages/components/card.jsx
+++ src/pages/components/card.jsx
@@ -6,28 +6,14 @@ import CardActions from '@mui/joy/CardActions';
 import CircularProgress from '@mui/joy/CircularProgress';
 import Typography from '@mui/joy/Typography';
 import SvgIcon from '@mui/joy/SvgIcon';
-
-export default function CardInvertedColors() {
+import { FaDollarSign } from "react-icons/fa";
+function CardInvertedColors() {
   return (
     <Card variant="solid" color="primary" invertedColors>
       <CardContent orientation="horizontal">
-        <CircularProgress size="lg" determinate value={20}>
-          <SvgIcon>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
-              />
-            </svg>
-          </SvgIcon>
-        </CircularProgress>
+        <div>
+            <FaDollarSign />
+        </div>
         <CardContent>
           <Typography level="body-md">Gross profit</Typography>
           <Typography level="h2">$ 432.6M</Typography>
@@ -44,3 +30,5 @@ export default function CardInvertedColors() {
     </Card>
   );
 }
+
+export default CardInvertedColors;
