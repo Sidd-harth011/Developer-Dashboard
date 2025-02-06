18ca09fe1f02b48c41a5b8385fa6efe116b4904e
Karan Yadav <karan342772@gmail.com>
2025-02-06 19:06:42 +0530

    frontend connection

--- developer dashboard backend/index.js
+++ developer dashboard backend/index.js
@@ -8,8 +8,13 @@ Connect_Mongo()
 
 
 const app = express();
+// connetion frontend
+app.use(express.static(path.join(__dirname,"../developer dashboard frontend/data")))
 
-
+app.get('*',(res,req)=>{
+    res.sendFile(path.join(__dirname,"../developer dashboard frontend/data",'index.html'))
+})
+// end connecting frontend
 app.listen(5000,()=>{
     console.log('the server is running');
 })
