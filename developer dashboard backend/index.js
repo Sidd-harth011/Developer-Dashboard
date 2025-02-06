1708cf8d53915e9d40bb3044fee1964f6fd98571
Karan Yadav <karan342772@gmail.com>
2025-02-06 20:52:31 +0530

    connection

--- developer dashboard backend/index.js
+++ developer dashboard backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require("path")
 const Connect_Mongo = require('./Mongo/mongo_config')
 //database connection
 Connect_Mongo()
@@ -9,10 +10,10 @@ Connect_Mongo()
 
 const app = express();
 // connetion frontend
-app.use(express.static(path.join(__dirname,"../developer dashboard frontend/data")))
+app.use(express.static(path.join(__dirname,"../developer-dashboard-frontend/data")))
 
 app.get('*',(res,req)=>{
-    res.sendFile(path.join(__dirname,"../developer dashboard frontend/data",'index.html'))
+    res.res.sendFile(path.join(__dirname,"../developer-dashboard-frontend/data",'index.html'))
 })
 // end connecting frontend
 app.listen(5000,()=>{
